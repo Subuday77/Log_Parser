328fb73567b276c122412a264a5bf2792f7007a5
Subuday77 <subuday.bagatur@gmail.com>
2020-09-22 23:37:40 +0300

    before release

--- src/main/java/com/logParser/logParser/rest/ParseController.java
+++ src/main/java/com/logParser/logParser/rest/ParseController.java
@@ -15,14 +15,23 @@ public class ParseController {
     @PostMapping("/timestamps")
     public ResponseEntity<?> parseForTs(@RequestBody String logToParse) throws JSONException {
         String answer = "";
+        String lineToParse = "";
         if (logToParse.isBlank()) {
             answer = "Empty log field";
         } else {
             String[] lines = logToParse.split("\n");
             for (String line : lines) {
-                if (line.contains("Got operator response")) {
-                    answer = answer.concat(parseLine(line));
-                   
+                if (line.contains("Got operator response") || lineToParse.contains("Got operator response")) {
+                    if (line.indexOf("{") > 0 && line.indexOf("}") > 0) {
+                        answer = answer.concat(parseLine(line));
+                    } else {
+                        lineToParse = lineToParse.concat(line);
+                        if (lineToParse.indexOf("{") > 0 && lineToParse.indexOf("}") > 0) {
+                            answer = answer.concat(parseLine(lineToParse));
+                            lineToParse = "";
+                        }
+                    }
+
                 }
             }
         }
@@ -30,12 +39,14 @@ public class ParseController {
     }
 
     private static String parseLine(String line) throws JSONException {
+
         line = line.substring(line.indexOf("{"));
         JSONObject toParse = new JSONObject(line);
         try {
-            return (toParse.getString("transactionId") + "\t" + toParse.getString("timestamp")+"\n");
+            return (toParse.getString("transactionId") + "\t" + toParse.getString("timestamp") + "\n");
         } catch (JSONException e) {
             return "";
         }
+
     }
 }
